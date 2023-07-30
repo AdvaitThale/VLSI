0131c5107d714358dee15bc027f273f9ecbf9edb
AdvaitThale <67235361+AdvaitThale@users.noreply.github.com>
2023-07-30 22:59:18 +0530

    Shift Register (#2)

---
+++ Shift_Register/Shift_Register.srcs/sources_1/new/Universal_ShiftReg.vhd
@@ -0,0 +1,35 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 30.07.2023 12:15:45
+-- Design Name: 
+-- Module Name: Universal_ShiftReg - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+
+entity Universal_ShiftReg is
+--  Port ( );
+end Universal_ShiftReg;
+
+architecture Behavioral of Universal_ShiftReg is
+
+begin
+
+
+end Behavioral;
