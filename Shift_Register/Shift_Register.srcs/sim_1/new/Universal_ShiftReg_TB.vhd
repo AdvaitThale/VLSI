45a275b0133532e70eeab59defbea9a95494f6d1
AdvaitThale <67235361+AdvaitThale@users.noreply.github.com>
2023-07-31 22:28:10 +0530

    Shift Register (#4)

--- Shift_Register/Shift_Register.srcs/sim_1/new/Universal_ShiftReg_TB.vhd
+++ Shift_Register/Shift_Register.srcs/sim_1/new/Universal_ShiftReg_TB.vhd
@@ -24,12 +24,62 @@ use IEEE.STD_LOGIC_1164.ALL;
 
 
 entity Universal_ShiftReg_TB is
---  Port ( );
 end Universal_ShiftReg_TB;
 
 architecture Behavioral of Universal_ShiftReg_TB is
-
+    component shiftregister
+    port(
+         CLK : IN std_logic;
+         LeftIn : IN std_logic;
+         RightIn : IN std_logic;
+         RST : IN std_logic;
+         MODE : IN std_logic_vector (1 downto 0);
+         I : IN std_logic_vector (3 downto 0);
+         Q : OUT std_logic_vector (3 downto 0));
+         end component;
+         
+     --INPUTS
+     signal CLK : std_logic := '0';
+     signal LeftIn : std_logic := '0';
+     signal RightIn : std_logic := '0';
+     signal RST : std_logic := '0';
+     signal MODE : std_logic_vector(1 downto 0) := (others => '0');
+     signal I : std_logic_vector(3 downto 0) := (others => '0');
+     
+     --OUTPUTS
+     signal Q : std_logic_vector(3 downto 0);
 begin
+    uut: shiftregister port map(
+    CLK => CLK,
+    LeftIn => LeftIn,
+    RightIn => RightIn,
+    RST => RST,
+    MODE => MODE,
+    I => I,
+    Q => Q);
+    
+process(CLK, RST)
+begin
+RST <= '1';
+RST <= '0' after 50ns;
+CLK <= not CLK after 10ns;
+end process;
 
-
+process
+    begin
+        I <= "1010"; 
+        wait for 100ns;
+        MODE <= "00"; 
+        wait for 100ns;
+        MODE <= "01"; 
+        wait for 100ns;
+        MODE <="10";
+        wait for 100ns;
+        RightIn <= '1'; 
+        wait for 100ns;
+        MODE <="11"; 
+        wait for 100ns;
+        LeftIn <= '1';
+        wait for 100ns;
+end process;
 end Behavioral;
--- Shift_Register/Shift_Register.srcs/sources_1/new/Universal_ShiftReg.vhd
+++ Shift_Register/Shift_Register.srcs/sources_1/new/Universal_ShiftReg.vhd
@@ -21,15 +21,32 @@
 
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
-
+use IEEE.STD_LOGIC_arith.ALL;
+use IEEE.STD_LOGIC_unsigned.ALL;
 
 entity Universal_ShiftReg is
---  Port ( );
+  Port (CLK, LeftIn, RightIn, RST: IN std_logic;
+        MODE: IN std_logic_vector (1 downto 0);
+        I: IN std_logic_vector (3 downto 0);
+        Y: OUT std_logic_vector (3 downto 0));
 end Universal_ShiftReg;
 
 architecture Behavioral of Universal_ShiftReg is
-
 begin
-
-
-end Behavioral;
+process(CLK, RST)
+variable Y_TMP: std_logic_vector (3 downto 0);
+    begin
+    if RST = '1' then
+    Y_TMP := "0000";
+elsIF(CLK = '1' AND CLK' EVENT) then
+    case mode is    
+    when "00" => Y_TMP := Y_TMP;
+    when "01" => Y_TMP := I;
+    when "10" => Y_TMP := Y_TMP(2 downto 0) & RightIn; -- LEFT Shift
+    when "11" => Y_TMP := LeftIn & Y_TMP(3 downto 1); -- RIGHT Shift
+    when others => null;
+    end case;
+  end if;
+  Y <= Y_TMP;    
+end process;
+end behavioral;
